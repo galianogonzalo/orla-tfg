61bcc1cbc2ec20a9d3a9063d5d3473556c1468cd
gonzalo <1700476@alu.murciaeduca.es>
2024-05-29 00:22:29 +0200

    Formulario basico crear-persona

--- src/app/components/crear-persona/crear-persona.component.ts
+++ src/app/components/crear-persona/crear-persona.component.ts
@@ -1,47 +1,43 @@
 import { Component, inject } from '@angular/core';
 import { Storage, ref, uploadBytesResumable } from '@angular/fire/storage';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-persona',
   standalone: true,
   imports: [ReactiveFormsModule],
-  template: `
-    <h2>Formulario creación de persona para orla</h2>
-    <div>
-      <input type="file" accept="images/*" (change)="changeInput($event)"/>
-    </div>
-
-    <h2>Añade personas a tus orlas</h2>
-    <div class="row row justify-content-center">      
-      <div class="form-container col-12 col-md-8 col-lg-4">
-        <form action="">
-          
-        </form>
-      </div>
-    </div>
-  `,
+  templateUrl: `./crear-persona.component.html`,
   styles: ``
 })
 export class CrearPersonaComponent {
-  
-  /* Declarar formulario */
-  myForm = new FormGroup({
 
-  })
-  
-  
-  
+  crearPersonaForm: FormGroup
+
   file!: File
 
   private readonly _storage = inject(Storage)
+
+
+  constructor(){
+    this.crearPersonaForm = new FormGroup([
+      /* nombre: new FormControl(), */
+
+    ])
+  }
+  
+  submit(): void {
+    this.uploadFile()
+    console.log(this.crearPersonaForm.value)
+  }
+  
+  
+  
 
   changeInput(event: Event){
     console.log(this._storage)
     const input = event.target as HTMLInputElement
     if(input.files){
-      this.file = input.files[0]
-      this.uploadFile()
+      this.file = input.files[0]     
     }
     
   }
