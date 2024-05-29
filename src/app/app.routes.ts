da3692a63ecb29c4d3cf7c27d2c6522d5b6d4578
gonzalo <1700476@alu.murciaeduca.es>
2024-05-29 21:12:57 +0200

    crear curso, listar cursos y alumnos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,16 @@ import { ListaOrlasComponent } from './components/lista-orlas/lista-orlas.compon
 import { CrearOrlaComponent } from './components/crear-orla/crear-orla.component';
 import { CrearPersonaComponent } from './components/crear-persona/crear-persona.component';
 import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { CrearCursoComponent } from './components/crear-curso/crear-curso.component';
+import { CursosComponent } from './components/cursos/cursos.component';
 
 export const routes: Routes = [
     {path: "inicio", component: HomeComponent, title: "Inicio"},
     {path: "login", component: LoginComponentComponent, title: "Login"}, 
     {path: "mis_orlas", component: ListaOrlasComponent, title: "Mis orlas"},
+    {path: "mis_cursos", component: CursosComponent, title: "Mis cursos"},
     {path: "crear_orla", component: CrearOrlaComponent, title: "Crear orla"},
+    {path: "crear_curso", component: CrearCursoComponent, title: "Crear curso"},
     {path: "crear_persona", component: CrearPersonaComponent, title: "Crear persona"},
     {path: "", redirectTo: "inicio", pathMatch:"full"},
     {path: "**", component: Page404Component, title: "Error 404"}
